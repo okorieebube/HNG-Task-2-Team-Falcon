8baa58b2a88372c2a2523c75eb9e8258a4870792
okorieebube <53199415+okorieebube@users.noreply.github.com>
2020-06-06 20:34:13 +0100

    Update HNG-05192.php
    i added a space before my email

--- scripts/HNG-05192.php
+++ scripts/HNG-05192.php
@@ -6,7 +6,7 @@ $language = 'PHP';
 $email = 'okorieebube1@gmail.com';
 
 function slack_details($full_name,$id,$language,$email){
-    return "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$language." for stage 2 task.". $email;
+    return "Hello World, this is ".$full_name." with HNGi7 ID ".$id." using ".$language." for stage 2 task. ". $email;
 }
 $returned = slack_details($full_name,$id,$language,$email);
 print $returned;
